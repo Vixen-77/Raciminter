0514e344173fb58bddc33d49397921e859f6fd2e
hikiuzrx <zch92142@gmail.com>
2025-02-28 12:46:06 +0100

    react router setup i'll give u tips how to proceed later

---
+++ src/components/layouts/RootLayout.jsx
@@ -0,0 +1,31 @@
+import { Outlet,NavLink } from "react-router-dom";
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <>
+        <div className="flex justify-evenly gap-3 text-xl text-white">
+           <NavLink to={"/"}>
+                <div>
+                    Home
+                </div>
+            </NavLink>
+            <NavLink to={"/about"}>
+                <div>
+                    About
+                </div>
+            </NavLink>
+            <NavLink to={"/contact"}>
+                <div>
+                    Contact
+                </div>
+            </NavLink> 
+      </div> 
+      <div>
+        <Outlet/>
+        </div>   
+    </>
+  )
+}
+
+export default RootLayout
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='text-2xl text-white'>
+      Contact
+    </div>
+  )
+}
+
+export default Contact
